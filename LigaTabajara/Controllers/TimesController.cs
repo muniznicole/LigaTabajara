f3d20e38667e799b91ab4378b545080e6433c7c5
muniznicole <nicoleamuniz@gmail.com>
2025-04-18 13:44:34 -0300

    Estilização com TailwindCSS e CRUD de Time

---
+++ LigaTabajara/Controllers/TimesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LigaTabajara.Models;
+
+namespace LigaTabajara.Controllers
+{
+    public class TimesController : Controller
+    {
+        private LigaTabajaraContext db = new LigaTabajaraContext();
+
+        // GET: Times
+        public ActionResult Index()
+        {
+            return View(db.Times.ToList());
+        }
+
+        // GET: Times/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Time time = db.Times.Find(id);
+            if (time == null)
+            {
+                return HttpNotFound();
+            }
+            return View(time);
+        }
+
+        // GET: Times/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Times/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TimeId,Nome,Cidade,Estado,AnoFundacao,Estadio,CapacidadeEstadio,CorUniformePrimaria,CorUniformeSecundaria,Status")] Time time)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Times.Add(time);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(time);
+        }
+
+        // GET: Times/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Time time = db.Times.Find(id);
+            if (time == null)
+            {
+                return HttpNotFound();
+            }
+            return View(time);
+        }
+
+        // POST: Times/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TimeId,Nome,Cidade,Estado,AnoFundacao,Estadio,CapacidadeEstadio,CorUniformePrimaria,CorUniformeSecundaria,Status")] Time time)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(time).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(time);
+        }
+
+        // GET: Times/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Time time = db.Times.Find(id);
+            if (time == null)
+            {
+                return HttpNotFound();
+            }
+            return View(time);
+        }
+
+        // POST: Times/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Time time = db.Times.Find(id);
+            db.Times.Remove(time);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
